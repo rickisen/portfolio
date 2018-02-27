27dc7149df951cfcaffd4bc8b9139de533b44bb3
rickisen <rickard.petersen@gmail.com>
2018-02-27 09:19:47 -0800

    pass butter cms token to client

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './containers/app';
 
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+
 import 'sanitize.css/sanitize.css';
 import './index.css';
+
+const env = runtimeEnv();
+console.log('env', env);
 
 const target = document.querySelector('#root');
 
